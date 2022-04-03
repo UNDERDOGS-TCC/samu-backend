26e3ca3d100f1614d07992c6ca75d1b6ef385a35
ouwargui <guikemon2@gmail.com>
2022-04-02 22:40:45 -0300

    feat: add morgan to app

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,8 @@
 import {Express} from 'express';
 import bodyParser from './bodyParser';
+import morgan from './morgan';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(morgan);
 };
