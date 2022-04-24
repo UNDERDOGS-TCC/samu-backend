be410b342339171bc9f9f1e6a5348d89094d2aad
ouwargui <guikemon2@gmail.com>
2022-04-24 03:59:57 -0300

    feat: change response code to 200

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -8,23 +8,23 @@ export default {
 
     if (!email || !password) {
       res
-        .status(400)
-        .json({message: 'Email or password missing!', success: false});
+        .status(200)
+        .json({message: 'Email ou senha faltando', success: false});
     } else {
       const usersCollection = await mongodb.getCollection('users');
       const user = await usersCollection.findOne({email: email});
       if (!user) {
         res
-          .status(400)
-          .json({message: 'Email or password incorrect!', success: false});
+          .status(200)
+          .json({message: 'Email ou senha incorretos', success: false});
       } else {
         if (user.password !== password) {
           res
-            .status(400)
-            .json({message: 'Email or password incorrect!', success: false});
+            .status(200)
+            .json({message: 'Email ou senha incorretos', success: false});
         } else {
           res.status(200).json({
-            message: 'User validated!',
+            message: 'User validado com sucesso',
             success: true,
             user,
           });
