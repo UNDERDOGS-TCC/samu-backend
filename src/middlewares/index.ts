e95f1f78538d573c0b50704ea23e5c2184cd4274
ouwargui <guikemon2@gmail.com>
2022-05-14 01:16:00 -0300

    feat: remove caching

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,11 @@
 import {Express} from 'express';
+import nocache from 'nocache';
 import bodyParser from './bodyParser';
 import morgan from './morgan';
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(morgan);
+  app.set('etag', false);
+  app.use(nocache());
 };
