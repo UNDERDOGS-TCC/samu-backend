488eadd1443f1d93bf63164e87be69ef115fc848
Caique Sobral <caiquelsobral@gmail.com>
2022-06-01 23:15:55 -0300

    Fix: minor fixes

--- src/helpers/samuLocation.ts
+++ src/helpers/samuLocation.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 export const closestSamuLocation = async (
-  latitude: number = -23.4896833,
-  longitude: number = -46.3754173,
+  latitude: number,
+  longitude: number,
+  radius: number,
 ): Promise<{lat: number; lng: number}> => {
   const apiKey = String(process.env.API_KEY);
   const config = {
     method: 'get',
-    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Cgeometry&input=samu%20serviço%20de%20emergencia&inputtype=textquery&locationbias=circle%3A5000%${latitude}%2C${longitude}&key=${apiKey}`,
+    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Cgeometry&input=samu%20servico%20de%20emergencia&inputtype=textquery&locationbias=circle%3A${radius}%${latitude}%2C${longitude}&key=${apiKey}`,
     headers: {},
   };
   await axios(config)
